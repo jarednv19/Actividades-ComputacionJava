d344fb64765f3c09219bf2c93f9cbc70e7ebe5cd
jarednv19 <112534053+jarednv19@users.noreply.github.com>
2022-10-12 00:58:36 -0500

    Update Deck.java

--- Deck.java
+++ Deck.java
@@ -1,5 +1,5 @@
 package Actividad5.java.Actividad8;
-/*  Actividad 8    Computación en Java
+/*  Actividad 9    Computación en Java
 *   Carlos Jared Nuñez Viera  #2886129
 */
 import java.io.IOException;
@@ -9,38 +9,83 @@ import java.util.Scanner;
 public class Deck {
     static Scanner leer = new Scanner(System.in);
     Random rand = new Random();
+    static int opcion;
 
     public static void main (String[] args) throws IOException{
-        shuffle();
-        System.out.println("\n");
-        head();
-        System.out.println("\n");
-        pick();
-        System.out.println("\n");
-        hand();
-        System.out.println("\n");
+        showMenu();
     }
 
+    public static void showMenu (){
+        System.out.println("Bienvenido a Poker!\nSelecciona una opción:");
+        System.out.println("1 Mezclar deck\n2 Sacar una carta\n3 Carta al azar\n4 Generar una mano de 5 cartas\n0 Salir\n");
+        opcion = leer.nextInt();
+        switch (opcion){
+            case 1:{
+                shuffle();
+                break;
+            }
+            case 2:{
+                head();
+                break;
+            }
+            case 3:{
+                pick();
+                break;
+            }
+            case 4:{
+                hand();
+                break;
+            }
+            case 0:{
+                break;
+            }
+            default:
+                System.out.println("Opción no valida. Intenta de nuevo");
+                opcion = leer.nextInt();
+                switch (opcion){
+                    case 1:{
+                        shuffle();
+                        break;
+                    }
+                    case 2:{
+                        head();
+                        break;
+                    }
+                    case 3:{
+                        pick();
+                        break;
+                    }
+                    case 4:{
+                        hand();
+                        break;
+                    }
+                    case 0:{
+                        break;
+                    }
+                }
+        } 
+    }
+    
     public static void shuffle (){
         System.out.println("Se mezcló el Deck");
     }
 
     public static void head (){
-        System.out.println("Tréboles, Negro, 8");
+        System.out.println("Corazones, Rojo, 7");
         System.out.println("Quedan 51 cartas en el deck");
     }
 
     public static void pick (){
-        System.out.println("Corazones, Rojo, 3");
+        System.out.println("Picas, Negro, Q");
         System.out.println("Quedan 50 cartas en el deck");
     }
 
     public static void hand (){
-        System.out.println("Picas, Negro, J");
-        System.out.println("Diamantes, Rojo, 5");
-        System.out.println("Picas, Negro, 9");
-        System.out.println("Corazones, Rojo, K");
-        System.out.println("Tréboles, Negro, 2");
+        System.out.println("Tréboles, Negro, 3");
+        System.out.println("Corazones, Rojo, 6");
+        System.out.println("Tréboles, Negro, K");
+        System.out.println("Picas, Negro, 4");
+        System.out.println("Diamantes, Rojo, 2");
         System.out.println("Quedan 45 cartas en el deck");
     }
 }
